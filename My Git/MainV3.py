babad621b78d06e5220fbf9cdcbb817b8101fb23
RodrigoGCoelhoo <rodrigogc4@al.insper.edu.br>
2020-06-08 16:33:53 -0300

    Proj

--- My Git/MainV3.py
+++ My Git/MainV3.py
@@ -1,5 +1,5 @@
 import pygame
-import random
+import random, math
 from Configurações import *
 from spritesv2 import *
 
@@ -15,42 +15,47 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
         self.font_name = pygame.font.match_font(FONT_NAME)
-        self.abs_x = 0
         self.load_data()
+        self.end_time = 0
+        self.bool_contato = False
     
     def load_data(self):
         self.dir = os.path.dirname(__file__)
         img_dir = os.path.join(self.dir, 'Tiles')
-        self.spritesheet_enemy = Spritesheet(os.path.join(img_dir,SPRITESHEET_ENEMY))
+        self.spritesheet_enemy = Spritesheet(os.path.join(img_dir, SPRITESHEET_ENEMY))
+        self.spritesheet_keys = Spritesheet(os.path.join(img_dir, SPRITESHEE_KEYS))
     
     def new(self):
         #Inicia o jogo
         self.todas_sprites = pygame.sprite.Group()
         self.platforms = pygame.sprite.Group()
-        self.background = pygame.sprite.Group()
         self.enemys = pygame.sprite.Group()
-        
-        assets = load_assets(imagem1)
-        
-        for row in range(len(MAP)):
-            for column in range(len(MAP[row])):
-                tile_type = MAP[row][column]
-                if tile_type == 3:
-                    tile = Tile(assets[tile_type], row, column)
-                    self.background.add(tile)
-                    self.todas_sprites.add(tile) 
-                    
-                else:
-                    tile = Tile(assets[tile_type], row, column)
-                    self.platforms.add(tile)
-                    self.todas_sprites.add(tile) 
-        
         self.player = Player(self)
-        self.todas_sprites.add(self.player)    
-        self.e = Enemy(self, 350, ALTURA - 40, 200)
+        self.todas_sprites.add(self.player)
+        self.grupo_setas = pygame.sprite.Group()
+        self.grupo_setas_certo = pygame.sprite.Group()
+        self.grupo_setas_errado = pygame.sprite.Group()
+
+        for i in range(0,5000, TILE_SIZE):
+            p1 = Platforms(i)
+            self.platforms.add(p1)
+            self.todas_sprites.add(p1)
+        
+        for i in range(0,5000, TILE_SIZE):
+            p1 = Platforms2(i)
+            self.platforms.add(p1)
+            self.todas_sprites.add(p1)
+            
+        for i in range(0,5000, TILE_SIZE):
+            p1 = Platforms3(i)
+            self.platforms.add(p1)
+            self.todas_sprites.add(p1)
+            
+        self.e = Enemy(self, 500, ALTURA - 40, 200)
         self.todas_sprites.add(self.e)
         self.enemys.add(self.e)
 
+        
         self.run()
      
     def run(self):
@@ -64,6 +69,7 @@ class Game:
              self.draw()
     
     def update(self):
+        global FPS
         #Atualiza o loop
         self.todas_sprites.update()
         if self.player.vel.y > 0:
@@ -72,7 +78,14 @@ class Game:
                 self.player.pos.y = hits[0].rect.top 
                 self.player.vel.y = 0 
                 
-       
+        if self.player.rect.right >= LARGURA/2:
+            self.player.pos.x -= abs(self.player.vel.x)
+            for plat in self.platforms:
+                plat.rect.x -= abs(self.player.vel.x + 0.5*self.player.acc.x)
+            
+            for enemy in self.enemys:
+                enemy.pos.x -= abs(self.player.vel.x + 0.5*self.player.acc.x)
+        
         for enemy in self.enemys:        
             if enemy.vel.y > 0:
                 hits = pygame.sprite.spritecollide(enemy, self.platforms, False)
@@ -80,7 +93,44 @@ class Game:
                     enemy.pos.y = hits[0].rect.top
                     enemy.vel.y = 0
         
-          
+        if self.player.rect.right < LARGURA/2:         
+            self.player.pos.x += abs(self.player.vel.x)
+            for plat in self.platforms:
+                plat.rect.x += abs(self.player.vel.x + 0.5*self.player.acc.x)
+
+            for enemy in self.enemys:
+                enemy.pos.x += abs(self.player.vel.x + 0.5*self.player.acc.x)
+
+        for enemy in self.enemys:
+            if self.distancia(self.player, enemy) < 150 and not self.bool_contato:
+                FPS = 20
+                self.bool_contato = True
+                self.end_time = pygame.time.get_ticks() + 3000
+                self.lista_setas_tela = []
+                self.pos_seta = 0
+                self.var_seta = 0
+                for i in range(50,550,100):
+                    seta = Setas(self, i, self.var_seta)
+                    self.lista_setas_tela.append(seta.retorno())
+                    self.grupo_setas.add(seta)
+                    self.todas_sprites.add(seta)
+                    self.var_seta += 1
+                    #print(self.lista_setas_tela)
+
+        self.current_time = pygame.time.get_ticks()
+        if self.end_time < self.current_time:
+            FPS = 60
+            self.bool_contato = False
+            for seta in self.grupo_setas:
+                self.grupo_setas.remove(seta)
+                self.todas_sprites.remove(seta)
+            for seta in self.grupo_setas_certo:
+                self.grupo_setas_certo.remove(seta)
+                self.todas_sprites.remove(seta)
+            for seta in self.grupo_setas_errado:
+                self.grupo_setas_errado.remove(seta)
+                self.todas_sprites.remove(seta)
+                
     def events(self):
         #Eventos do loop
         for evento in pygame.event.get():
@@ -91,14 +141,53 @@ class Game:
 
             if evento.type == pygame.KEYDOWN:
                 if evento.key == pygame.K_SPACE:
-    
                     self.player.jump()
                 
                 if (evento.key == pygame.K_ESCAPE):
                     if self.playing:
                         self.playing = False
                         self.running = False
-                                                    
+
+                if evento.key == pygame.K_UP:
+                    for seta in self.grupo_setas:
+                        if seta.cod == self.pos_seta:
+                            if seta.sentido == 'cima':
+                                seta.acerto()
+                                break
+                            else:
+                                seta.erro()
+                                break
+
+                if evento.key == pygame.K_DOWN:
+                    for seta in self.grupo_setas:
+                        if seta.cod == self.pos_seta:
+                            if seta.sentido == 'baixo':
+                                seta.acerto()
+                                break
+                            else:
+                                seta.erro()
+                                break
+
+                if evento.key == pygame.K_RIGHT:
+                    for seta in self.grupo_setas:
+                        if seta.cod == self.pos_seta:
+                            if seta.sentido == 'direita':
+                                seta.acerto()
+                                break
+                            else:
+                                seta.erro()
+                                break
+
+                if evento.key == pygame.K_LEFT:
+                    for seta in self.grupo_setas:
+                        if seta.cod == self.pos_seta:
+                            if seta.sentido == 'esquerda':
+                                seta.acerto()
+                                break
+                            else:
+                                seta.erro()
+                                break
+
     def draw(self):
         #Desenha as imagens
         self.tela.fill(GREEN2)
@@ -138,20 +227,27 @@ class Game:
                 if (evento.type == pygame.KEYDOWN) and (evento.key == pygame.K_ESCAPE):
                     waiting = False
                     self.running = False
-                    
+                
+           
     def draw_text(self, text, size, color, x, y):
         font = pygame.font.Font(self.font_name, size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
         text_rect.midtop = (x, y)
         self.tela.blit(text_surface, text_rect)
-                
+
+    def distancia(self, obj1, obj2):
+        self.dif_x = math.fabs(obj1.pos.x) - math.fabs(obj2.pos.x)
+        self.dif_y = math.fabs(obj1.pos.y) - math.fabs(obj2.pos.y)
+        self.pit = math.sqrt(self.dif_x**2 + self.dif_y**2)
+        return self.pit
+
 g = Game()
 g.show_start_screen()
 
 while g.running:
+
     g.new()
     g.show_go_screen()
-    
     
 pygame.quit() 
